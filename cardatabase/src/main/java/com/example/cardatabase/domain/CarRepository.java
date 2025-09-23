d62f38f86f0c6583909baf58ace6738f70fd63b8
taewonKang1 <edward0317@naver.com>
2025-09-23 17:36:44 +0900

    20250923 springboot

--- cardatabase/src/main/java/com/example/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/CarRepository.java
@@ -1,7 +1,14 @@
 package com.example.cardatabase.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+import java.util.List;
+
+@RepositoryRestController
 public interface CarRepository extends JpaRepository<Car, Long> {
-    Owner owner(Owner owner);
+    List<Car> findByBrand(@Param("brand") String brand);
+
+    List<Car> findByColor(@Param("color") String color);
 }
---
+++ cardatabase/src/main/java/com/example/cardatabase/web/CarController.java
@@ -0,0 +1,20 @@
+package com.example.cardatabase.web;
+
+import com.example.cardatabase.domain.Car;
+import com.example.cardatabase.domain.CarRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController {
+    private final CarRepository carRepository;
+
+    public CarController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    @GetMapping("/cars")
+    public Iterable<Car> getCars() {
+        return carRepository.findAll();
+    }
+}
